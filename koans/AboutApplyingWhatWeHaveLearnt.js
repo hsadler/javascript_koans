0bad1353af25016e2977990db81270f8567654c1
Harry Sadler <harrysadlermusic@gmail.com>
2015-05-07 21:24:33 -0700

    completed "multiples of 3 or 5" section

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -64,14 +64,26 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var isMultipleOfThreeOrFive = function(num) {
+      if(num % 3 === 0 || num % 5 === 0) {
+        return true;
+      } else return false;
+    };
 
-    expect(233168).toBe(FILL_ME_IN);
+    var sumNums = function(memo, x) { return memo + x; };
+
+    /* try chaining range() and reduce() */
+    var sum = _(_.range(1000)).chain()
+                  .filter(isMultipleOfThreeOrFive)
+                  .reduce(sumNums, 0)
+                  .value();
+
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
